53da6e126b9b93f4e40b00c3ead4837367963ef3
yuchong-pan <panyuchong@gmail.com>
2018-05-25 23:49:39 -0700

    Added Quick.java

---
+++ 10analysis/Quick.java
@@ -0,0 +1,41 @@
+import edu.princeton.cs.algs4.*;
+
+public class Quick {
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo, j = hi + 1;
+        while (true) {
+            while (less(a[++i], a[lo])) if (i == hi) break;
+            while (less(a[lo], a[--j])) if (j == lo) break;
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+
+    public static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        if (v == w) return false;
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+        Quick.sort(a, 0, a.length - 1);
+        for (int i = 0; i < a.length; i++) {
+            StdOut.println(a[i]);
+        }
+    }
+}
