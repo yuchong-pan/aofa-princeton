6e70458acdb5f40a5ff4feb47375bf4afe669bee
yuchong-pan <panyuchong@gmail.com>
2018-05-29 22:39:58 -0700

    Added QuickSeq.java

---
+++ 20recurrence/QuickSeq.java
@@ -0,0 +1,25 @@
+import edu.princeton.cs.algs4.*;
+
+public class QuickSeq implements Sequence {
+    private final double[] c;
+
+    public QuickSeq(int maxN) {
+        c = new double[maxN + 1];
+        c[0] = 0;
+        for (int N = 1; N <= maxN; N++) {
+            c[N] = (N + 1) * c[N - 1] / N + 2;
+        }
+    }
+
+    public double eval(int N) {
+        return c[N];
+    }
+
+    public static void main(String[] args) {
+        int maxN = Integer.parseInt(args[0]);
+        QuickSeq c = new QuickSeq(maxN);
+        for (int i = 0; i <= maxN; i++) {
+            StdOut.println(c.eval(i));
+        }
+    }
+}
